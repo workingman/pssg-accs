c6e3bd55d9b5afe0d81a03d59d55360ee403c564
workingman <geoffrey.routledge@icloud.com>
2017-11-28 14:02:56 -0800

    simplify data load

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:latest
 COPY . /root
 WORKDIR /root
+RUN npm install
 #RUN grunt
 #CMD ["node", "super_complex_app.js"]
--- service.js
+++ service.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var data = {
+  "user1": {
+    "name": "Geoff Routledge",
+    "department": "Cloud Pursuit",
+    "designation": "Architect",
+    "id": 1
+  },
+  "user2": {
+    "name": "Jim Knutsen",
+    "department": "Sales",
+    "designation": "Account Executive",
+    "id": 2
+  }
+}
 
 app.get('/getUsers', function(req, res) {
-  fs.readfile(__dirname + '/' + "user.json", 'utf8', function(err, data) {
     console.log( data );
-    res.end( data );
-  });
+    res.send( data );
 });
 
-var server = app.listen( process.env.PORT, function() {
+var server = app.listen( 8881, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Node REST service application listening at port: %s", port );
